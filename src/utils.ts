16a7375637abf56e18bfde7f69de53faa27e9c29
Athma Chidambaravasi <athma@Athmas-Mac-mini.local>
2026-09-13 15:58:17 -0700

    add capitalizeString

---
+++ src/utils.ts
@@ -0,0 +1,5 @@
+function capitalizeString(str: string): string {
+    const trimmedStr = str.trim();
+    return `${trimmedStr.charAt(0).toUpperCase()}${trimmedStr.slice(1)}`;
+}
+export { capitalizeString };
