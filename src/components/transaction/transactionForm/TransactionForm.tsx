06051f6ad3a49b518b9c800f4c550a4e0ff0242b
Athma Chidambaravasi <athma@Athmas-Mac-mini.local>
2026-09-13 17:46:56 -0700

    add initial hooks

--- src/components/transaction/transactionForm/TransactionForm.tsx
+++ src/components/transaction/transactionForm/TransactionForm.tsx
@@ -1,6 +1,44 @@
+import { useEffect, useReducer, useRef } from "react";
+import { transactionFormReducer } from "./reducers";
+import { initialTransactionFormState } from "./state";
+
 type TransactionFormProps = {};
 
 function TransactionForm() {
+    const [
+        transactionFormState,
+        transactionFormDispatch,
+    ] = useReducer(
+        transactionFormReducer,
+        // backupStateFromErrorHOC ?? initialRegisterState,
+        initialTransactionFormState,
+    );
+    const {
+        amountCents,
+        cacheWorkerMaybe,
+        category,
+        dateTime,
+        fetchWorkerMaybe,
+        forageWorkerMaybe,
+        isLoading,
+        merchant,
+        notes,
+        paymentMethod,
+        responseDataMaybe,
+        safeErrorMaybe,
+        tags,
+        type,
+    } = transactionFormState;
+
+    const amountCentsInputRef = useRef<HTMLInputElement | null>(null);
+    useEffect(() => {
+        amountCentsInputRef.current?.focus?.();
+    }, []);
+
+    if (safeErrorMaybe.isSome()) {
+        throw safeErrorMaybe.value;
+    }
+
     return null;
 }
 
